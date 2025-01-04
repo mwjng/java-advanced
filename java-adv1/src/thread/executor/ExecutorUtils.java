71d006d4c85ce97a5f2f533a665296dfb2f36497
wmw04087 <wmw04087@gmail.com>
2025-01-04 14:35:43 +0900

    Executor shutdown, 풀 전략, 예외정책

--- java-adv1/src/thread/executor/ExecutorUtils.java
+++ java-adv1/src/thread/executor/ExecutorUtils.java
@@ -19,4 +19,18 @@ public abstract class ExecutorUtils {
             log(executorService);
         }
     }
+
+    // 추가
+    public static void printState(ExecutorService executorService, String taskName) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            final int pool = poolExecutor.getPoolSize();
+            final int active = poolExecutor.getActiveCount();
+            final int queuedTasks = poolExecutor.getQueue().size();
+            final long completedTask = poolExecutor.getCompletedTaskCount();
+            log(taskName + " -> [pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks +
+                ", completedTask=" + completedTask + "]");
+        } else {
+            log(executorService);
+        }
+    }
 }
---
+++ java-adv1/src/thread/executor/poolsize/PoolSizeMainV2.java
@@ -0,0 +1,30 @@
+package thread.executor.poolsize;
+
+import thread.executor.RunnableTask;
+
+import java.util.concurrent.*;
+
+import static thread.executor.ExecutorUtils.printState;
+import static util.MyLogger.log;
+
+public class PoolSizeMainV2 {
+
+    public static void main(String[] args) {
+        final ExecutorService es = Executors.newFixedThreadPool(2);
+/*
+        final ExecutorService es = new ThreadPoolExecutor(2, 2,
+            0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+*/
+
+        log("pool 생성");
+        printState(es);
+
+        for (int i = 0; i < 6; i++) {
+            String taskName = "task" + i;
+            es.execute(new RunnableTask(taskName));
+            printState(es, taskName);
+        }
+        es.close();
+        log("== shutdown 완료 ==");
+    }
+}
